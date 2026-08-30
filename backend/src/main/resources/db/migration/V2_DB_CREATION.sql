8352ba84e6b2b2a018cbf668276481d69da34351
zorgon <tgemelos@gmail.com>
2026-08-30 17:38:21 +0300

    adding db properties

--- backend/src/main/resources/db/migration/V2_DB_CREATION.sql
+++ backend/src/main/resources/db/migration/V2_DB_CREATION.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE db123_todo_app;
-USE db123_todo_app;
+-- CREATE DATABASE todo_mysql_tasos;
+USE todo_mysql_tasos;
 
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
